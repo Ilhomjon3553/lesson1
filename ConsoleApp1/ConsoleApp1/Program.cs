29da853d4dafb1adb71feddf5a41c32ac56bb69f
yusupovilhomjon <ilhomjon3553@gmail.com>
2024-12-30 23:41:55 +0300

    Initial commit with existing project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите сторону квадрата (a):");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double S = a * a;
+            Console.WriteLine($"Площадь квадрата: {S}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Integer2/Integer2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер файла в байтах");
+            long size = Convert.ToInt64(Console.ReadLine());
+            long kilobytes = size / 1024;
+            Console.WriteLine($"Количество полных килобайтов: {kilobytes}");
+            Console.ReadKey();
+        }
+    }
+}
